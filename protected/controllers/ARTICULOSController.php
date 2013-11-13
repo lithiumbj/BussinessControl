98bd0a9154af693a3c61277e73ecce9b33b63d52
borja <borja@borja-HP-Pavilion-g7-Notebook-PC>
2013-11-13 18:00:38 +0100

    -Añadido sistema de seguridad para que ningún usuario pueda acceder

--- protected/controllers/ARTICULOSController.php
+++ protected/controllers/ARTICULOSController.php
@@ -28,15 +28,7 @@ class ARTICULOSController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
+				'actions'=>array('index','view','create','update', 'delete','admin'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
--- protected/controllers/FACTURASController.php
+++ protected/controllers/FACTURASController.php
@@ -28,16 +28,8 @@ class FACTURASController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update', 'print'),
+				'actions'=>array('index','view','create','update', 'delete','admin','print'),
 				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete', 'print'),
-				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
