4b163af03efaa1d8928655802d29999035b88822
borja <borja@borja-HP-Pavilion-g7-Notebook-PC>
2013-11-15 19:33:11 +0100

    -> Ahora se ven los nombres de los clientes en las factuas

--- protected/views/fACTURAS/index.php
+++ protected/views/fACTURAS/index.php
@@ -13,7 +13,22 @@ $this->menu=array(
 ?>
 
 <h1>Facturases</h1>
+<?php
 
+    //Importar el controlador de Clientes
+    Yii::import('application.controllers.CLIENTESController');
+    //Almacenar el objeto del cliente entero
+    //Cambiar los datos del dataProvider de id de cliente por su nombre real
+    $dataDelProvider = $dataProvider->getData();
+    for($i=0;$i<count($dataDelProvider);$i++){
+        //Obtener los datos del cliente
+        $clienteModel = CLIENTESController::findClienteById($dataDelProvider[$i]['idCliente']);
+        //Reasignar los datos correspondientes en el modlo
+        $dataDelProvider[$i]['idCliente'] = $clienteModel->Nombre.' '.$clienteModel->Apellidos;
+    }
+    //Reinsertar el modelo 
+    $dataProvider->setData($dataDelProvider);
+?>
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
